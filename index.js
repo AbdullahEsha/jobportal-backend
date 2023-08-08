28a18afa3cf0c77b89a9985e3bb22e7bb6e86858
AbdullahIsha <shahariaresha@gmail.com>
2023-08-09 02:36:06 +0600

    fixed

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const app = require("./app");
-const dbConnect = require("./utils/dbConn");
+const app = require('./app')
+const dbConnect = require('./utils/dbConn')
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.get("/", (req, res) => {
-  res.send("hello world! test check operational....");
-});
+app.get('/', (req, res) => {
+  res.send('hello world! test check operational....')
+})
 
 app.listen(PORT, () => {
   console.log(
-    `nazara backend server is running on port http://localhost:${PORT}`
-  );
-  dbConnect();
-});
+    `jobportal backend server is running on port http://localhost:${PORT}`,
+  )
+  dbConnect()
+})
